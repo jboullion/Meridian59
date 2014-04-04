04d092bc3de640295b5510e489b81f05a97a376f
Daenks <dankx0r@gmail.com>
2014-04-04 17:27:31 -0500

    blakserv now builds in VC2008SP1 all .\util projects now build in VS client compiles!

---
+++ clientd3d/prebuild.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Making trig with command: ..\bin\maketrig.exe .\
+..\bin\maketrig.exe .\
+echo Making pal  with command: ..\bin\makepal ..\blakston.pal .\
+echo 
+..\bin\makepal ..\blakston.pal .\
